8a38ec02a67140b22c71b50b35e178a6cefee976
yoon <jmyoon888@gmail.com>
2021-01-25 18:52:10 +0900

    프로그래스바

--- client/src/views/App.js
+++ client/src/views/App.js
@@ -4,6 +4,7 @@ import Table from'@material-ui/core/Table'
 import TableHead from'@material-ui/core/TableHead'
 import TableCell from '@material-ui/core/TableCell'
 import TableRow from '@material-ui/core/TableRow'
+import TableBody from'@material-ui/core/TableBody'
 import Paper from '@material-ui/core/Paper'
 import {makeStyles} from '@material-ui/core/styles'
 import CircularProgress from'@material-ui/core/CircularProgress'
@@ -27,38 +28,40 @@ const useStyle=makeStyles({
 function App() {
 
   
-  const [completed,setCompleted]=useState(0)
+  const [completed,setCompleted]=useState({progress:0})
   const [customer,setCustomer]=useState([])
 
   const progressFunc =()=>{
 
-   if(completed<=100){
-     setCompleted(completed+1)
+   if(completed.progress<=100){
+     completed.progress=parseInt(completed.progress+1)
    }else{
-     setCompleted(0)
+     completed.progress=1
    }
+   setCompleted({progress:completed.progress})
   }
   
-  
-  
   useEffect(() => {
-
-    setInterval(() => {
+    var inter=setInterval(() => {
       progressFunc()
-    }, 200);
-
+    }, 150);
     axios
     .get('/api/customers')
     .then(res=>setCustomer(res.data))
     .catch(err=>console.log(err))
-
+    completed.progress=0
+    clearInterval(inter)
+    
   }, [])
- 
+
   
+
+
   const classes =useStyle();
   return (
     <Suspense fallback={(<div>loading....</div>)}>
-      {completed}
+    
+     
       <Paper className={classes.root}>
         <Table className={classes.table}>
           <TableHead>
@@ -71,8 +74,17 @@ function App() {
                 <TableCell>생일</TableCell>
                 <TableCell>직업</TableCell>
               </TableRow>
-            {customer.map((value,index)=><Customer key={index} number={index+1} id={value.id} name={value.name} img={value.img} birth={value.birth} gender={value.gender} job={value.job} />)}
           </TableHead>
+
+          <TableBody>
+            
+            {customer.map((value,index)=><Customer key={index} number={index+1} id={value.id} name={value.name} img={value.img} birth={value.birth} gender={value.gender} job={value.job} />)}
+            <TableRow>
+              <TableCell colSpan="6" align="center">
+                  <CircularProgress className={classes.progress} variant="determinate" value={completed.progress}/>
+              </TableCell>
+            </TableRow>
+          </TableBody>
         </Table>
       </Paper>
     </Suspense>

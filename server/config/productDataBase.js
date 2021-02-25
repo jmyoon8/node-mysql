518cf34164816b4ca393cb0491538c15493e90ad
yoon <jmyoon888@gmail.com>
2021-02-25 17:24:00 +0900

    배포준비

--- server/config/productDataBase.js
+++ server/config/productDataBase.js
@@ -5,15 +5,14 @@ const connecting=mysql.createConnection({
     // host:"myaws.cf7smk2karlz.ap-northeast-2.rds.amazonaws.com",
     // user:"admin",
     // password:"dbswowls12!",
-    host:"localhost",
-    user:'root',
-    password:"1234",
-    port:"3306",
-    database:"management"
+    host:"us-cdbr-east-03.cleardb.com",
+    user:'b12d0cdac395f7',
+    password:"92f4f08b",
+    database:"heroku_a5bf4153d4f0a48"
 })
 //연결완료
 connecting.connect();
 
-
+// mysql://b12d0cdac395f7:92f4f08b@us-cdbr-east-03.cleardb.com/heroku_a5bf4153d4f0a48?reconnect=true
 //그리고 exports
 module.exports=connecting
--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ if(process.env.NODE_ENV==="production"){
         res.sendFile(path.resolve(__dirname,"../client","build","index.html"))
     })
 }
-
+console.log(process.env.NODE_ENV)
 
 
 
